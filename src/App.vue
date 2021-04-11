da0b1213e1aa851f0b863202ac883bf1d15ea3ce
Mariana Oliveira <olvr.mariana@gmail.com>
2021-04-10 21:08:43 -0300

    creates app wireframe

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/user">Perfil</router-link> |
-      <router-link to="/edit">Editar Perfil</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="app">
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-app-bar clipped-left app>
+          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-toolbar-title>Delve</v-toolbar-title>
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" clipped app>
+          <v-sheet rounded="lg">
+            <SideBar />
+          </v-sheet>
+        </v-navigation-drawer>
+        <v-row>
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+              class="d-flex justify-center align-center flex-row mb-6">
+              <router-link to="/user">Perfil</router-link> |
+              <router-link to="/edit">Editar Perfil</router-link>
+              <router-view />
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+import SideBar from '@/components/SideBar.vue';
+
+export default {
+  data: () => ({ drawer: null }),
+
+  components: {
+    SideBar,
+  },
+};
+</script>
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,17 +1,13 @@
 <template>
   <div class="user">
-    <HelloWorld msg="Olá, mundão!"/>
+    <h1>Perfil</h1>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default Vue.extend({
   name: 'User',
-  components: {
-    HelloWorld,
-  },
 });
 </script>
