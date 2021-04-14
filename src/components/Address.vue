234a781aa042095e131bd617f4eb1cffcbd17cc1
Mariana Oliveira <olvr.mariana@gmail.com>
2021-04-14 16:12:58 -0300

    feature(component): changes from code review

--- src/components/Address.vue
+++ src/components/Address.vue
@@ -3,16 +3,41 @@
     <v-text-field
       label="CEP"
       v-model="cep"
-      v-on:keyup.enter="search()"
+      v-on:keyup.enter="getPostalCode()"
       v-mask="'#####-###'"
       :rules="cepRules"
     ></v-text-field>
-    <v-btn color="primary" small rounded @click="search()">Pesquisar</v-btn>
-    <v-text-field label="Rua" v-model="street" :rules="sizeRules"></v-text-field>
-    <v-text-field label="Número" v-model="number" :rules="numberRules"></v-text-field>
+    <v-btn
+      color="primary"
+      small rounded
+      @click="getPostalCode()">Pesquisar
+    </v-btn>
 
-    <v-text-field label="Cidade" v-model="city" :rules="sizeRules"></v-text-field>
-    <v-btn color="primary" class="my-2" small rounded @click="save()">Salvar</v-btn>
+    <v-text-field
+      label="Rua"
+      v-model="street"
+      :rules="sizeRules">
+    </v-text-field>
+
+    <v-text-field
+      label="Número"
+      v-model="number"
+      :rules="numberRules">
+    </v-text-field>
+
+    <v-text-field
+      label="Cidade"
+      v-model="city"
+      :rules="sizeRules">
+    </v-text-field>
+
+    <v-btn
+      color="primary"
+      class="my-2"
+      small
+      rounded
+      @click="save()">Salvar
+    </v-btn>
   </v-form>
 </template>
 
@@ -26,13 +51,18 @@ export default Vue.extend({
     return {
       valid: true,
       cep: '',
-      cepRules: [(v) => v.length > 8 || 'CPF incompleto'],
+      cepRules: [(v: '') => v.length > 8 || 'CPF incompleto'],
       street: '',
-      sizeRules: [(v) => !!v || 'Insira pelo menos 3 caracteres'],
+      sizeRules: [(v: '') => !!v || 'Insira pelo menos 3 caracteres'],
       number: '',
-      numberRules: [(v) => !!v || 'Informe o número'],
+      numberRules: [(v: '') => !!v || 'Informe o número'],
       city: '',
     };
+  },
+  computed: {
+    form(): Vue & { validate: () => boolean } {
+      return this.$refs.form as Vue & { validate: () => boolean };
+    },
   },
   mounted() {
     if (localStorage.cep && localStorage.street && localStorage.number && localStorage.city) {
@@ -43,21 +73,27 @@ export default Vue.extend({
     }
   },
   methods: {
-    search() {
+    getPostalCode() {
       const { cep } = this;
-      cepService.get(`${cep}`).then((response) => {
-        this.street = response.data.logradouro;
-        this.city = response.data.cidade;
-      });
+      cepService.get(`${cep}`)
+        .then((response) => {
+          this.street = response.data.logradouro;
+          this.city = response.data.cidade;
+        }).catch((error) => {
+          this.errorHandler();
+          console.log(error);
+        });
     },
     save() {
-      if (this.$refs.form.validate()) {
+      if (this.form.validate()) {
         localStorage.cep = this.cep;
         localStorage.street = this.street;
         localStorage.number = this.number;
         localStorage.city = this.city;
-        console.log('salvou');
       }
+    },
+    errorHandler() {
+      alert('Ops, algo deu errado. Tente atualizar a página!');
     },
   },
 });
